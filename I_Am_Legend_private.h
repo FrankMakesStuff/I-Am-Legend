64a8c8bcd6dd65bd51b2424c1834fa3fa585cc8a
Frank Leslie <fleslee@gmail.com>
2020-06-22 22:13:41 -0400

    2nd remote commit
    moving stuff around

---
+++ I_Am_Legend_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef I_AM_LEGEND_PRIVATE_H
+#define I_AM_LEGEND_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.0.0.582"
+#define VER_MAJOR	0
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	582
+#define COMPANY_NAME	""
+#define FILE_VERSION	"0.0.0.582"
+#define FILE_DESCRIPTION	"I Am Legend game, inspired by Dwarf Fortress"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"I Am Legend"
+#define PRODUCT_VERSION	"0.0.0.582"
+
+#endif /*I_AM_LEGEND_PRIVATE_H*/
